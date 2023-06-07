4f958293decfc391ea7d1c940872974758b3464a
Swarnali Roy <swarnali.roy093@gmail.com>
2023-06-07 20:42:19 +0600

    test cases updated for faq page

--- __tests__/home.test.jsx
+++ __tests__/home.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "../src/app/page";
 import { faqItems } from "../src/app/page";
 
@@ -8,10 +8,24 @@ describe("Home component", () => {
   it("should render frequently asked questions", () => {
     const { getByText } = render(<Home />);
     expect(getByText("Frequently Asked Questions")).toBeInTheDocument();
+  });
 
+  it("should render all the faq headers", () => {
+    const { getByText } = render(<Home />);
     //checking if all the items are rendering properly
     faqItems.forEach((item) => {
-      expect(getByText(item.header)).toBeInTheDocument();
+      const headerElement = getByText(item.header);
+      expect(headerElement).toBeInTheDocument();
+    });
+  });
+
+  it("should render content body when clicked for all components", () => {
+    const { getByText } = render(<Home />);
+    faqItems.forEach((item) => {
+      const headerElement = getByText(item.header);
+      fireEvent.click(headerElement);
+      const bodyElement = getByText(item.content);
+      expect(bodyElement).toBeInTheDocument();
     });
   });
 });
